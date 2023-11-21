03da908e9fbbda6fbd0dd5611038680db4784924
nickolasvm <nickolas64vm@gmail.com>
2023-11-21 13:53:05 -0300

    feat: Implemented  LINQ query to retrive games types

--- src/TrybeGames/Database/TrybeGamesDatabase.cs
+++ src/TrybeGames/Database/TrybeGamesDatabase.cs
@@ -52,11 +52,17 @@ public class TrybeGamesDatabase
 
     }
 
-    // 8. Crie a funcionalidade de buscar todos os diferentes Tipos de jogos dentre os jogos cadastrados
     public List<GameType> GetGameTypes()
     {
-        // Implementar
-        throw new NotImplementedException();
+        var gamesByType = Games.GroupBy(game => game.GameType);
+        List<GameType> gameTypes = new();
+
+        foreach (var game in gamesByType)
+        {
+            gameTypes.Add(game.Key);
+        }
+
+        return gameTypes;
     }
 
     // 9. Crie a funcionalidade de buscar todos os estúdios de jogos junto dos seus jogos desenvolvidos com suas pessoas jogadoras
